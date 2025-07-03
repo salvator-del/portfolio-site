be55fde66e06ef71023a7bd72a4283c796a22633
salvator-del <65698466+salvator-del@users.noreply.github.com>
2025-07-03 16:56:56 +0300

    Add files via upload
    first commit

---
+++ portfolio-site/js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","Full-stack Developer","Web Designer","Python Developer","Infosec Analyst"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
